f98aaba51c43b5ebcfb36da7830aa29623f3bf9a
atemmel <temmeladam@gmail.com>
2021-05-05 16:58:29 +0200

    Heck

--- lab3/map.py
+++ lab3/map.py
@@ -3,41 +3,52 @@
 import json
 import geopandas as gpd
 import pandas as pd
+import numpy as np
 
-from bokeh.io import show
+from bokeh.io import show, curdoc
 from bokeh.plotting import figure
-from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, RangeSlider, GMapOptions, CDSView, IndexFilter
 from bokeh.palettes import brewer
+from bokeh.layouts import row, column
 
 df = pd.read_csv("./imdb_movie_metadata.csv")
-#df = df.dropna(how='any')
-df.loc[df['country'] == "USA"] = "United States of America"
-dict_country_number_of_movies = dict()
+df.loc[df['country'] == "USA", 'country'] = "United States of America"
 
-for i in df["country"].unique():
-    dict_country_number_of_movies[i] = 0
-
-for index, row in df.iterrows():
-    country = row['country']
-    try:
-        if len(country) > 0:
-            dict_country_number_of_movies[country] += 1
-    except TypeError as e:
-        print("lmao")
-    
-r_df = pd.DataFrame(list(dict_country_number_of_movies.items()), columns=["name", "number_of_movies"])
-#print(r_df)
+all_years = df["title_year"].unique()
+min_year = int(min(all_years))
+max_year = int(max(all_years))
 
 shapefile = "./shapefile.shp"
 gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))[["name", "geometry"]]
 
-merged = gdf.merge(r_df, left_on = 'name', right_on = 'name', how='left')
-merged['number_of_movies'] = merged['number_of_movies'].fillna(0)
+def gen_df_range(y1, y2):
+    dict_country_number_of_movies = dict()
 
-print(merged)
+    for i in df["country"].unique():
+        dict_country_number_of_movies[i] = 0
 
-merged_json = json.loads(merged.to_json())
-json_data = json.dumps(merged_json)
+    for index, r in df.iterrows():
+        country = r['country']
+        year = r['title_year']
+        try:
+            if len(country) > 0 and year >= y1 and year <= y2:
+                dict_country_number_of_movies[country] += 1
+        except TypeError as e:
+            print("lmao")
+        
+    return pd.DataFrame(list(dict_country_number_of_movies.items()), columns=["name", "number_of_movies"])
+
+r_df = gen_df_range(min_year, max_year)
+
+def transform_geojson(data):
+
+    merged = gdf.merge(r_df, left_on = 'name', right_on = 'name', how='left')
+    merged['number_of_movies'] = merged['number_of_movies'].fillna(0)
+
+    merged_json = json.loads(merged.to_json())
+    return json.dumps(merged_json)
+
+json_data = transform_geojson(r_df)
 
 def plot_map(json_data,plot_col,title):
 
@@ -46,12 +57,12 @@ def plot_map(json_data,plot_col,title):
     #Define a sequential multi-hue color palette.
     palette = brewer['Reds'][8]
     palette = palette[::-1]
-    color_mapper = LinearColorMapper(palette = palette, low = 0, high = 1)
+    color_mapper = LinearColorMapper(palette = palette, low = 0, high = 21)
 
-    tick_labels = {'0': '0%', '5': '5%', '10':'10%', '15':'15%', '20':'20%', '25':'25%', '30':'30%','35':'35%', '40': '>40%'}
+    tick_labels = {'0': '0', '5': '5', '10':'10', '15':'15', '20':'20', '21':'>20'}
 
     color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8,width = 500, height = 20,
-    border_line_color=None,location = (0,0), orientation = 'horizontal', major_label_overrides = tick_labels)
+        border_line_color=None,location = (0,0), orientation = 'horizontal', major_label_overrides = tick_labels)
 
     p = figure(title = title, plot_height = 600 , plot_width = 950, toolbar_location = None)
     p.xgrid.grid_line_color = None
@@ -61,8 +72,9 @@ def plot_map(json_data,plot_col,title):
               line_color = 'black', line_width = 0.25, fill_alpha = 1)
 
     p.add_layout(color_bar, 'below')
+    #slider = RangeSlider(title="Year: ", start=min_year, end=max_year, value=(0, 10), step=10)
+    #slider.on_change('value', callback)
+    #layout = column(p, slider)
+    curdoc().add_root(p)
 
-    #Display figure.
-    show(p)
-
-plot_map(json_data, "number_of_movies", "b")
+plot_map(json_data, "number_of_movies", "Number of movies")
